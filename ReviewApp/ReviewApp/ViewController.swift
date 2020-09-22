01afaabec4e33c3d2772695f44a230bce44b2e49
나성희 <sungh2na@gmail.com>
2020-09-22 23:35:32 +0900

    Calendar 추가
    FS Calendar 추가하고 날짜 출력

--- ReviewApp/ReviewApp/ViewController.swift
+++ ReviewApp/ReviewApp/ViewController.swift
@@ -4,16 +4,24 @@
 //
 //  Created by Y on 2020/09/22.
 //
-
+import FSCalendar
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FSCalendarDelegate {
 
+    @IBOutlet var calendar: FSCalendar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        calendar.delegate = self
     }
-
+    
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "EEEE MM-dd-YYYY"
+        let string = formatter.string(from: date)
+        print("\(string)")
+    }
 
 }
 
