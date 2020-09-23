f23104cd65d44663132f58f749e56c5b6a42df70
나성희 <sungh2na@gmail.com>
2020-09-23 18:15:38 +0900

    Collection view 확인

--- ReviewApp/ReviewApp/ReviewPlannerViewController.swift
+++ ReviewApp/ReviewApp/ReviewPlannerViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ReviewPlannerViewController: UIViewController, FSCalendarDelegate {
 
     @IBOutlet var calendar: FSCalendar!
+    @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,3 +26,49 @@ class ReviewPlannerViewController: UIViewController, FSCalendarDelegate {
 
 }
 
+//extension ReviewPlannerViewController {
+//    @objc private func adjustInputView(noti: Notification) {
+//        guard let userInfo = noti.userInfo else { return }
+//        // 키보드 높이에 따른 인풋뷰 위치 변경
+//    }
+//}
+
+extension ReviewPlannerViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // 섹션 몇개
+        return 2
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // 섹션별 아이템 몇개
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        // 커스텀 셀
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ReviewPlannerCell", for: indexPath) as? ReviewPlannerCell else {
+            return UICollectionViewCell()
+        }
+        return cell
+    }
+    
+    
+}
+
+extension ReviewPlannerViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        // 사이즈 계산하기
+        let width:CGFloat = collectionView.bounds.width
+        let height:CGFloat = 40
+        return CGSize(width: width, height: height)
+    }
+}
+
+class ReviewPlannerCell: UICollectionViewCell {
+    
+    @IBOutlet weak var checkButton: UIButton!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var progressLabel: UILabel!
+    
+    @IBOutlet weak var modifyButton: UIButton!
+    
+}
