0a4a1b9719a94db2d4c389846c719b10d4cfe1e7
나성희 <sungh2na@gmail.com>
2020-09-24 21:53:01 +0900

    컬렉션뷰 수정

--- ReviewApp/ReviewApp/ReviewPlannerViewController.swift
+++ ReviewApp/ReviewApp/ReviewPlannerViewController.swift
@@ -12,7 +12,7 @@ class ReviewPlannerViewController: UIViewController, FSCalendarDelegate {
     @IBOutlet var calendar: FSCalendar!
     @IBOutlet weak var collectionView: UICollectionView!
     
-    let reviewPlannerViewModel = ReviewViewModel()
+    let reviewPlannerViewModel = ReviewPlannerViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +25,7 @@ class ReviewPlannerViewController: UIViewController, FSCalendarDelegate {
         formatter.dateFormat = "EEEE MM-dd-YYYY"
         let string = formatter.string(from: date)
         print("\(string)")
+        // 날짜 선택시 발생하는 이벤트!
     }
 
 }
@@ -43,7 +44,11 @@ extension ReviewPlannerViewController: UICollectionViewDataSource {
     }
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // 섹션별 아이템 몇개
-        return reviewPlannerViewModel.todayTodo.count
+        if section == 0 {       // Today
+            return reviewPlannerViewModel.todayTodos.count
+        } else {                // upcoming
+            return reviewPlannerViewModel.upcomingTodos.count
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -74,18 +79,60 @@ class ReviewPlannerCell: UICollectionViewCell {
     
     @IBOutlet weak var checkButton: UIButton!
     @IBOutlet weak var descriptionLabel: UILabel!
-    @IBOutlet weak var progressLabel: UILabel!
-     
+    @IBOutlet weak var progressLabel: UILabel!  // 이것도 받아야함. 나중에 추가
+    @IBOutlet weak var deleteButton: UIButton!
+    @IBOutlet weak var strikeThroughView: UIView! // 할일 미뤘을 때.
+    
+    @IBOutlet weak var strikeThroughWidth: NSLayoutConstraint!
+    
     @IBOutlet weak var modifyButton: UIButton!
     
     var doneButtonTapHandler: (() -> Void)?
+    var deleteButtonTapHandler: (() -> Void)?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        reset()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        reset()
+    }
     
     func updateUI(todo: Todo) {
         // 셀 업데이트 하기
+        checkButton.isSelected = todo.isDone
+        descriptionLabel.text = todo.detail
+        descriptionLabel.alpha = todo.isDone ? 0.2 : 1
+        deleteButton.isHidden = todo.isDone  == false
+        showStrikeThrough(todo.isDone)      // 수정하기
+        
+    }
+    
+    private func showStrikeThrough(_ show: Bool) {
+        if show {
+            strikeThroughWidth.constant = descriptionLabel.bounds.width     // 줄 보여줘야 할 때
+        } else {
+            strikeThroughWidth.constant = 0                                 // 줄 감출 때
+        }
+    }
+    
+    func reset() {
+        descriptionLabel.alpha = 1
+        deleteButton.isHidden = true
     }
     
     @IBAction func checkButtonTapped(_ sender: Any) {
         // checkButton 처리
+        checkButton.isSelected = !checkButton.isSelected
+        let isDone = checkButton.isSelected
+        showStrikeThrough(isDone)                       // 수정하기
+        descriptionLabel.alpha = isDone ? 0.2 : 1
+        deleteButton.isHidden = !isDone
+        
     }
     
+    @IBAction func deleteButtonTapped(_ sender: Any) {
+    }
 }
