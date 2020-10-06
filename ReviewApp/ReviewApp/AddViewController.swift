681bf67daad26849f9e0e470b6510ef48701079c
나성희 <sungh2na@gmail.com>
2020-10-06 18:30:12 +0900

    actionSheet 추가

--- ReviewApp/ReviewApp/AddViewController.swift
+++ ReviewApp/ReviewApp/AddViewController.swift
@@ -14,34 +14,45 @@ protocol EditDelegate{
 class AddViewController: UIViewController {
     
     @IBOutlet weak var inputTextField: UITextField!
-    
+    @IBOutlet weak var intervalLabel: UILabel!
     var delegate: EditDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
     @IBAction func close(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
+    
     @IBAction func done(_ sender: Any) {
         if delegate != nil {
             delegate?.addTaskButtonTapped(inputTextField.text!)
         }
         dismiss(animated: true, completion: nil)
     }
-  
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    @IBAction func setInterval(_ sender: Any) {
+        let alert = UIAlertController(title:"복습 간격 선택", message: "원하는 복습 간격이 없을 경우 직접 입력", preferredStyle: .actionSheet)
+        let interval_1 =  UIAlertAction(title: "오늘, 1일, 3일, 7일, 15일", style: .default) {
+            (action) in self.intervalLabel.text = "오늘, 1일, 3일, 7일, 15일"
+        }
+        let interval_2 =  UIAlertAction(title: "오늘, 1일, 5일, 10일, 20일", style: .default) {
+            (action) in self.intervalLabel.text = "오늘, 1일, 5일, 10일, 30일"
+        }
+        let interval_3 =  UIAlertAction(title: "오늘, 1일, 7일, 15일, 30일", style: .default) {
+            (action) in self.intervalLabel.text = "오늘, 1일, 7일, 15일, 30일"
+        }
+        let interval_4 =  UIAlertAction(title: "직접입력", style: .default) {
+            (action) in
+        }
+        let cancel =  UIAlertAction(title: "취소", style: .cancel)
+        
+        alert.addAction(interval_1)
+        alert.addAction(interval_2)
+        alert.addAction(interval_3)
+        alert.addAction(interval_4)
+        alert.addAction(cancel)
+        present(alert, animated: true, completion: nil)
+        }
 }
