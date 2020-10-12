24848589630205e9d022eb12dbbcd0142d39ccf6
나성희 <sungh2na@gmail.com>
2020-10-12 23:54:11 +0900

    setting 만들기

---
+++ ReviewApp/ReviewApp/Setting.swift
@@ -0,0 +1,58 @@
+//
+//  Setting.swift
+//  ReviewApp
+//
+//  Created by Y on 2020/10/12.
+//
+import UIKit
+
+struct SettingInfo {
+    let section: Int
+    let data: String
+    
+    init(section: Int, data: String) {
+        self.section = section
+        self.data = data
+    }
+}
+
+class SettingViewModel {
+    var settingInfos: [SettingInfo] = [
+        SettingInfo(section: 0, data: "알림 설정"),
+        SettingInfo(section: 0, data: "스터디day 설정"),
+        SettingInfo(section: 1, data: "오픈소스"),
+        SettingInfo(section: 1, data: "문의")
+    ]
+//
+//    enum Section: Int, CaseIterable {
+//        case general
+//        case etc
+//        var title: String {
+//            switch self {
+//            case .general: return "일반"
+//            default: return "기타"
+//            }
+//        }
+//    }
+//
+    var generalDatas: [SettingInfo] {
+        return settingInfos.filter { $0.section == 0 }
+    }
+    
+    var etcDatas: [SettingInfo] {
+        return settingInfos.filter { $0.section == 1 }
+    }
+    
+    var numOfSection: Int {
+        return 2
+    }
+    
+    var numOfGeneralData: Int {
+        return generalDatas.count
+    }
+    
+    var numOEtcData: Int {
+        return etcDatas.count
+    }
+}
+
--- ReviewApp/ReviewApp/SettingViewController.swift
+++ ReviewApp/ReviewApp/SettingViewController.swift
@@ -8,22 +8,39 @@
 import UIKit
 
 class SettingViewController: UIViewController {
+    
+    let settingViewModel = SettingViewModel()
     @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension SettingViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "SettingCell", for: indexPath) as? SettingCell else {
+            return UITableViewCell()
+        }
+        
+        let settingInfo = settingViewModel.settingInfos(at: indexPath.row)
+        
+    }
+}
 
+extension SettingViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("---> \(indexPath.row)")
+    }
+}
+
+class SettingCell: UITableViewCell {
+    
 }
