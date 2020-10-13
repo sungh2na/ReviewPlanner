65c6d3df34fda7439e5e5c248766b53c86eef23d
나성희 <sungh2na@gmail.com>
2020-10-13 22:54:41 +0900

    no message

--- ReviewApp/ReviewApp/SettingViewController.swift
+++ ReviewApp/ReviewApp/SettingViewController.swift
@@ -8,30 +8,35 @@
 import UIKit
 
 class SettingViewController: UIViewController {
-    
+
+//    @IBOutlet weak var tableView: UITableView!
     let settingViewModel = SettingViewModel()
-    @IBOutlet weak var tableView: UITableView!
-    
+//
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
 }
 
 extension SettingViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 2
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "SettingCell", for: indexPath) as? SettingCell else {
             return UITableViewCell()
         }
-        
-        let settingInfo = settingViewModel.settingInfos(at: indexPath.row)
-        
+        var settingInfo: SettingInfo
+//        if indexPath.section == 0 {
+//            settingInfo = settingViewModel.generalDatas[indexPath.row]
+//        } else {
+//            settingInfo = settingViewModel.etcDatas[indexPath.row]
+//        }
+        settingInfo = settingViewModel.settingInfos[indexPath.row]
+
+        cell.updateUI(settingInfo: settingInfo)
+
+        return cell
     }
 }
 
@@ -42,5 +47,13 @@ extension SettingViewController: UITableViewDelegate {
 }
 
 class SettingCell: UITableViewCell {
-    
+    @IBOutlet weak var settingData: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    func updateUI(settingInfo: SettingInfo) {
+        settingData.text = settingInfo.data
+    }
 }
