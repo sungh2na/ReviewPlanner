980b95decd7022f1767c15a7e51342de107d3d49
나성희 <sungh2na@gmail.com>
2020-10-22 23:49:06 +0900

    static tableview 만듦

---
+++ ReviewApp/ReviewApp/TableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  TableViewController.swift
+//  ReviewApp
+//
+//  Created by Y on 2020/10/22.
+//
+
+import Foundation
+import UIKit
+
+class TableViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
