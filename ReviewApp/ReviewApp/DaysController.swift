ea7ca6a8e99ecbe3efbd505c1162fb61cee865a3
나성희 <sungh2na@gmail.com>
2020-10-24 23:55:26 +0900

    요일체크

--- ReviewApp/ReviewApp/DaysController.swift
+++ ReviewApp/ReviewApp/DaysController.swift
@@ -5,10 +5,16 @@
 //  Created by Y on 2020/10/18.
 //
 
+import Foundation
 import UIKit
 
-class DaysContoller : UIViewController {
+class DaysContoller : UITableViewController {
+    var days: [Int] = [0,1,2,3,4,5,6]
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = super.tableView(tableView, cellForRowAt: indexPath)
+        return cell
     }
 }
