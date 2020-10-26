2399ff084fde7494c3337497dc5cf308bd10da89
나성희 <sungh2na@gmail.com>
2020-10-26 23:56:20 +0900

    no message

--- ReviewApp/ReviewApp/TableViewController.swift
+++ ReviewApp/ReviewApp/TableViewController.swift
@@ -14,19 +14,33 @@ class TableViewController: UITableViewController {
         // days 수정
         super.viewDidLoad()
     }
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "DaysController" {
-            if let secondView = segue.destination as? DaysController {
-                secondView.delegate = self.storedDays
-            }
-        }
-    }
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if segue.identifier == "DaysController" {
+//            if let secondView = segue.destination as? DaysController {
+//                secondView.delegate = self.storedDays
+//            }
+//        }
+//    }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(indexPath)
-//        performSegue(withIdentifier: "showModify", sender: indexPath.row)
-        if(indexPath.row == 1) {
-            performSegue(withIdentifier: "DaysController", sender: nil)
+//        print(indexPath)
+//        if indexPath.row == 1 {
+//            performSegue(withIdentifier: "DaysController", sender: nil)
+//        }
+        if indexPath.section == 0, indexPath.row == 0 {
+            if let controller = self.storyboard?.instantiateViewController(identifier: "NoticeController") {
+                self.navigationController?.pushViewController(controller, animated: true)
+            }
+        }
+        if indexPath.section == 0, indexPath.row == 1 {
+            if let controller = self.storyboard?.instantiateViewController(identifier: "DaysController") {
+                self.navigationController?.pushViewController(controller, animated: true)
+            }
+        }
+        if indexPath.section == 1, indexPath.row == 1 {
+            if let controller = self.storyboard?.instantiateViewController(identifier: "OpensourceController") {
+                self.navigationController?.pushViewController(controller, animated: true)
+            }
         }
     }
 }
