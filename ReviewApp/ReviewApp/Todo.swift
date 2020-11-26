27aa6e455c4c2c57cee027c67713b257b5fbac50
나성희 <sungh2na@gmail.com>
2020-11-26 23:55:22 +0900

    todo 정리

--- ReviewApp/ReviewApp/Todo.swift
+++ ReviewApp/ReviewApp/Todo.swift
@@ -54,10 +54,6 @@ class TodoManager {
         TodoManager.reviewId += 1
     }
     
-//    func addTodo(_ todo: Todo) {
-//        todos.append(todo)
-//        saveTodo()
-//    }
     func addNewTodo(_ todo: Todo) {
         let newTodo = NewTodo(context: context)
         newTodo.id = Int16(todo.id)
@@ -71,14 +67,6 @@ class TodoManager {
         retrieveNewTodo()
     }
     
-//    func deleteTodo(_ todo: Todo) {
-//        todos = todos.filter { existingTodo in           // 같은 계획 모두 삭제, 반복 계획만 삭제하는 코드 id != id , progress != progress
-//            return existingTodo.id != todo.id
-//        }
-//        setProgress(todo)
-//        saveTodo()
-//    }
-//
     func deleteNewTodo(_ todo: NewTodo) {
         let object = context.object(with: todo.objectID)
         context.delete(object)
@@ -86,13 +74,6 @@ class TodoManager {
         saveNewTodo()
         retrieveNewTodo()
     }
-    
-//    func deleteAllTodo(_ todo: Todo) {
-//        todos = todos.filter { existingTodo in
-//            return existingTodo.reviewId != todo.reviewId
-//        }
-//        saveTodo()
-//    }
     
     func deleteAllNewTodo(_ todo: NewTodo) {
         let reviewId = todo.reviewId
@@ -108,14 +89,6 @@ class TodoManager {
         retrieveNewTodo()
     }
     
-//    func delayTodo(_ todo: Todo) {
-//        guard let index = todos.firstIndex(of: todo) else { return }
-//        let today = todo.date
-//        let tomorrow = today.addingTimeInterval(Double(86400))
-//        todos[index].update(isDone: todo.isDone, detail: todo.detail, date: tomorrow, reviewNum: todo.reviewNum, reviewTotal: todo.reviewTotal)
-//        saveTodo()
-//    }
-    
     func delayNewTodo(_ todo: NewTodo) {
         let today = todo.date
         let tomorrow = today?.addingTimeInterval(Double(86400))
@@ -123,21 +96,6 @@ class TodoManager {
         saveNewTodo()
         retrieveNewTodo()
     }
-    
-//    func updateTodo(_ todo: Todo) {
-//        guard let index = todos.firstIndex(of: todo) else { return }
-//        todos[index].update(isDone: todo.isDone, detail: todo.detail, date: todo.date, reviewNum: todo.reviewNum, reviewTotal: todo.reviewTotal)
-//        saveTodo()
-//    }
-    
-//    func updateAllTodo(_ todo: Todo) {      // 코드 수정하기
-//        for index in 0 ..< todos.count {
-//            if todos[index].reviewId == todo.reviewId {
-//                todos[index].update(isDone: todos[index].isDone, detail: todo.detail, date: todos[index].date, reviewNum: todos[index].reviewNum, reviewTotal: todos[index].reviewTotal)
-//            }
-//        }
-//        saveTodo()
-//    }
     
     func updateNewTodo(_ todo: NewTodo) {
         let predicate = NSPredicate(format: "reviewId == %d", todo.reviewId)
@@ -150,19 +108,6 @@ class TodoManager {
         saveNewTodo()
         retrieveNewTodo()
     }
-    
-    
-//    func setProgress(_ todo: Todo) {        // 코드 수정하기
-//        for index in 0 ..< todos.count {
-//            if todos[index].reviewId == todo.reviewId {
-//                if todos[index].reviewNum < todo.reviewNum {
-//                    todos[index].update(isDone: todos[index].isDone, detail: todos[index].detail, date: todos[index].date, reviewNum: todos[index].reviewNum, reviewTotal: todos[index].reviewTotal - 1)
-//                } else {
-//                    todos[index].update(isDone: todos[index].isDone, detail: todos[index].detail, date: todos[index].date, reviewNum: todos[index].reviewNum - 1, reviewTotal: todos[index].reviewTotal - 1)
-//                }
-//            }
-//        }
-//    }
     
     func setNewProgress(_ todo: NewTodo) {
         let reviewId = todo.reviewId
@@ -184,30 +129,15 @@ class TodoManager {
         }
     }
     
-//    func saveTodo() {
-//        Storage.store(todos, to: .documents, as: "todos.json")
-//    }
-    
     func saveNewTodo() {
         print("home: \(NSHomeDirectory())")
         try! context.save()
     }
-//    func retrieveTodo() {
-//        todos = Storage.retrive("todos.json", from: .documents, as: [Todo].self) ?? []
-//        let lastId = todos.last?.id ?? 0
-//        let reviewId = todos.last?.reviewId ?? 0
-//        TodoManager.lastId = lastId
-//        TodoManager.reviewId = reviewId
-//    }
     
     func retrieveNewTodo() {
         let request: NSFetchRequest<NewTodo> = NewTodo.fetchRequest()
         self.newTodos = try! context.fetch(request)
     }
-    
-//    func todayTodo(_ date: Date) {
-//        todayTodos = todos.filter { $0.date.toString(format: "yyyy MM dd") == date.toString(format: "yyyy MM dd") }
-//    }
     
     func todayNewTodo(_ date: Date) {
         let newDate = date as NSDate
@@ -227,65 +157,29 @@ class TodoManager {
 class ReviewPlannerViewModel {
     private let manager = TodoManager.shared
     
-//    var todos: [Todo] {
-//        return manager.todos
-//    }
-    
-//    var todayTodos: [Todo] {
-//        return manager.todayTodos
-//    }
-//
     var todayNewTodos: [NewTodo] {
         return manager.todayNewTodos
     }
-    
-//    func addTodo(_ todo: Todo) {
-//        manager.addTodo(todo)
-//    }
-    
+
     func addNewTodo(_ todo: Todo) {
         manager.addNewTodo(todo)
     }
-    
-//    func deleteTodo(_ todo: Todo) {
-//        manager.deleteTodo(todo)
-//    }
-    
+
     func deleteNewTodo(_ todo: NewTodo) {
         manager.deleteNewTodo(todo)
     }
-    
-//    func deleteAllTodo(_ todo: Todo) {
-//        manager.deleteAllTodo(todo)
-//    }
-    
+
     func deleteAllNewTodo(_ todo: NewTodo) {
         manager.deleteAllNewTodo(todo)
     }
-    
-//    func delayTodo(_ todo: Todo) {
-//        manager.delayTodo(todo)
-//    }
     
     func delayNewTodo(_ todo: NewTodo) {
         manager.delayNewTodo(todo)
     }
     
-//    func updateTodo(_ todo: Todo) {
-//        manager.updateTodo(todo)
-//    }
-    
-//    func updateAllTodo(_ todo: Todo) {
-//        manager.updateAllTodo(todo)
-//    }
-    
     func updateNewTodo(_ todo: NewTodo) {
         manager.updateNewTodo(todo)
     }
-    
-//    func loadTasks() {
-//        manager.retrieveTodo()
-//    }
     
     func loadNewTasks() {
         manager.retrieveNewTodo()
@@ -294,11 +188,7 @@ class ReviewPlannerViewModel {
     func saveTasks() {
         manager.saveNewTodo()
     }
-    
-//    func todayTodo(_ date: Date) {
-//        manager.todayTodo(date)
-//    }
-    
+
     func todayNewTodo(_ date: Date) {
         manager.todayNewTodo(date)
     }
