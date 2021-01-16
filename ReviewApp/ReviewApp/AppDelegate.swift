d6a2ea5d409dd1c2ea5758a1ce8af803aaeedc51
나성희 <sungh2na@gmail.com>
2021-01-16 23:33:06 +0900

    cloudkit 연결하기

--- ReviewApp/ReviewApp/AppDelegate.swift
+++ ReviewApp/ReviewApp/AppDelegate.swift
@@ -36,29 +36,30 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: - Core Data stack
 
     lazy var persistentContainer: NSPersistentContainer = {
-        /*
-         The persistent container for the application. This implementation
-         creates and returns a container, having loaded the store for the
-         application to it. This property is optional since there are legitimate
-         error conditions that could cause the creation of the store to fail.
-        */
-        let container = NSPersistentContainer(name: "ReviewApp")
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                 
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
-                fatalError("Unresolved error \(error), \(error.userInfo)")
+        
+//        let container = NSPersistentContainer(name: "ReviewApp")
+        let container = NSPersistentCloudKitContainer(name: "ReviewApp")
+        
+        // Create a store description for a CloudKit-backed local store
+        let cloudStoreLocation = URL(fileURLWithPath: "/path/to/cloud.store")
+        let cloudStoreDescription = NSPersistentStoreDescription(url: cloudStoreLocation)
+        cloudStoreDescription.configuration = "Cloud"
+
+        // Set the container options on the cloud store
+        cloudStoreDescription.cloudKitContainerOptions =
+            NSPersistentCloudKitContainerOptions(containerIdentifier: "iCloud.SungheeNa.ReLePlanner")
+            
+        // Update the container's list of store descriptions
+        container.persistentStoreDescriptions = [cloudStoreDescription]
+        
+        // Load both stores
+        container.loadPersistentStores { storeDescription, error in
+            guard error == nil else {
+                fatalError("Could not load persistent stores. \(error!)")
             }
-        })
+        }
+        
+        
         return container
     }()
 
