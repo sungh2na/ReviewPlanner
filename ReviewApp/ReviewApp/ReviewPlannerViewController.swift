c24b9405278ff260381457211afe6fdeca740c65
나성희 <sungh2na@gmail.com>
2021-01-29 01:29:49 +0900

    today 오류 수정
    viewWillAppear, willEnterForeground에 today 수정 코드 추가

--- ReviewApp/ReviewApp/ReviewPlannerViewController.swift
+++ ReviewApp/ReviewApp/ReviewPlannerViewController.swift
@@ -36,18 +36,18 @@ class ReviewPlannerViewController: UIViewController, AddDelegate, ModifyDelegate
         self.tableView.tableFooterView = UIView()
         self.calendar.select(Date())
         self.calendar.scope = .month
+        
         NotificationCenter.default.addObserver(self, selector: #selector(willEnterForeground), name: UIApplication.willEnterForegroundNotification, object: nil)
-
         selectedDate = self.calendar.selectedDate ?? Date() // 이벤트 표시
         setNotification()
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        print("viewWillAppear")
+        self.calendar.today = Date()
     }
     
     @objc func willEnterForeground() {
-        print("willEnterForeground")
+        self.calendar.today = Date()
     }
     
     func noticeLabelOn() {
